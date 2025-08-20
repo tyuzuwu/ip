ec827de03c22ecfca194454a33e360671963652d
zheng hong <tewzhenghong@outlook.com>
2025-08-20 23:13:36 +0800

    Add support for list

--- src/main/java/Hachiware.java
+++ src/main/java/Hachiware.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Hachiware {
@@ -14,6 +15,9 @@ public class Hachiware {
         //Use scanner to get user input
         Scanner scan = new Scanner(System.in);
 
+        //Create array to hold tasks
+        ArrayList<String> tasks = new ArrayList<>();
+
         //While loop to repeatedly get user input
         while (true) {
             String input = scan.nextLine();
@@ -25,12 +29,23 @@ public class Hachiware {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("-----------------------------------------------");
                 break;
+            } else if (input.equals("list")) {
+                //Print list
+                System.out.println("-----------------------------------------------");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i+1) + ". " + tasks.get(i));
+                }
+                System.out.println("-----------------------------------------------");
+            } else {
+                //Add to tasks
+                tasks.add(input);
+
+                //Normal Echo
+                System.out.println("-----------------------------------------------");
+                System.out.println("added: " + input);
+                System.out.println("-----------------------------------------------");
             }
 
-            //Normal Echo
-            System.out.println("-----------------------------------------------");
-            System.out.println(input);
-            System.out.println("-----------------------------------------------");
         }
 
         scan.close();
