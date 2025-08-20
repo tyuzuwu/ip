0eac7bcc82e09a5b59463412d9c4535fb43ffa2a
zheng hong <tewzhenghong@outlook.com>
2025-08-20 16:44:06 +0800

    Add greeting

---
+++ src/main/java/Hachiware.java
@@ -0,0 +1,13 @@
+public class Hachiware {
+    public static void main(String[] args) {
+        String logo =
+                " /\\_/\\  \n"
+                + "( o.o )  \n"
+                + " > ^ <   \n";
+        System.out.println(logo);
+        System.out.println("Hello~! I'm Hachiware \nWhat can I do for you?");
+        System.out.println("------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("------------------------------");
+    }
+}
