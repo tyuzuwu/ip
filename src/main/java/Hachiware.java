8b953b160fe087fb9874e56e5f8c1e2ea0ae739b
zheng hong <tewzhenghong@outlook.com>
2025-08-20 22:42:15 +0800

    Add support for echo

--- src/main/java/Hachiware.java
+++ src/main/java/Hachiware.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Hachiware {
     public static void main(String[] args) {
         String logo =
@@ -5,9 +7,33 @@ public class Hachiware {
                 + "( o.o )  \n"
                 + " > ^ <   \n";
         System.out.println(logo);
+        System.out.println("-----------------------------------------------");
         System.out.println("Hello~! I'm Hachiware \nWhat can I do for you?");
-        System.out.println("------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("------------------------------");
+        System.out.println("-----------------------------------------------");
+
+        //Use scanner to get user input
+        Scanner scan = new Scanner(System.in);
+
+        //While loop to repeatedly get user input
+        while (true) {
+            String input = scan.nextLine();
+
+            //If bye, exit loop
+            if (input.equals("bye")) {
+
+                System.out.println("-----------------------------------------------");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("-----------------------------------------------");
+                break;
+            }
+
+            //Normal Echo
+            System.out.println("-----------------------------------------------");
+            System.out.println(input);
+            System.out.println("-----------------------------------------------");
+        }
+
+        scan.close();
+
     }
 }
