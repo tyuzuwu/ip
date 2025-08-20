ac31e01e25833b138f09d185f7d687cd27e10e4f
zheng hong <tewzhenghong@outlook.com>
2025-08-21 00:00:52 +0800

    Add support for mark as done

--- src/main/java/Hachiware.java
+++ src/main/java/Hachiware.java
@@ -15,8 +15,8 @@ public class Hachiware {
         //Use scanner to get user input
         Scanner scan = new Scanner(System.in);
 
-        //Create array to hold tasks
-        ArrayList<String> tasks = new ArrayList<>();
+        //Create array of tasks to hold tasks
+        ArrayList<Task> tasks = new ArrayList<>();
 
         //While loop to repeatedly get user input
         while (true) {
@@ -29,16 +29,35 @@ public class Hachiware {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("-----------------------------------------------");
                 break;
-            } else if (input.equals("list")) {
+            } else if (input.equals("list")) { //Listing tasks
                 //Print list
                 System.out.println("-----------------------------------------------");
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < tasks.size(); i++) {
                     System.out.println((i+1) + ". " + tasks.get(i));
                 }
                 System.out.println("-----------------------------------------------");
+            } else if (input.startsWith("mark ")) { //Marking tasks
+                int taskIndex = Integer.parseInt(input.substring(5)) - 1; //5th Character of the input onwards, -1 because of indexing
+                Task currentTask = tasks.get(taskIndex);
+                currentTask.markAsDone();
+                System.out.println("-----------------------------------------------");
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println(currentTask);
+                System.out.println("-----------------------------------------------");
+
+            } else if (input.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(input.substring(7)) - 1; //7th Character of the input onwards, -1 because of indexing
+                Task currentTask = tasks.get(taskIndex);
+                currentTask.markAsNotDone();
+                System.out.println("-----------------------------------------------");
+                System.out.println("OK, I've marked this task as not done yet: ");
+                System.out.println(currentTask);
+                System.out.println("-----------------------------------------------");
             } else {
                 //Add to tasks
-                tasks.add(input);
+                Task newTask = new Task(input);
+                tasks.add(newTask);
 
                 //Normal Echo
                 System.out.println("-----------------------------------------------");
