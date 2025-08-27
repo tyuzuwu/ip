f424f58dfbf3b4f528cd2f76055ba322c4c68c3d
zheng hong <tewzhenghong@outlook.com>
2025-08-27 16:10:52 +0800

    Add support for Date and Times

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,15 +1,24 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String from;
-    protected String to;
+    protected LocalDateTime from;
+    protected LocalDateTime to;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDateTime.parse(from, FORMATTER);
+        this.to = LocalDateTime.parse(to, FORMATTER);
+
+        if (this.to.isBefore(this.from)) {
+            throw new IllegalArgumentException("Ending time cannot be before starting time.");
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(from: " + from + " to: " + to + ")";
+        return "[E]" + super.toString() + "(from: " + from.format(FORMATTER) + " to: " + to.format(FORMATTER) + ")";
     }
 }
